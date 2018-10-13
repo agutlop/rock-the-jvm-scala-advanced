2cacd42ccabed489c695ef5588debea19e92e884
agutlop <gutierrez.lopez.ant@gmail.com>
2018-10-13 21:50:19 +0200

    end dark sugar

---
+++ src/lectures/part1as/DarkSugar.scala
@@ -0,0 +1,104 @@
+package lectures.part1as
+
+import scala.util.Try
+
+object DarkSugar extends App{
+
+  // syntax sugar #1: methods with single param
+  def singleArgumentMethod(arg: Int): String = s"$arg little ducks..."
+
+  val description = singleArgumentMethod {
+    // write some complex code
+    42
+  }
+
+  val aTryInstance = Try { // java's try {...}
+    throw new RuntimeException
+  }
+
+  List(1, 2, 3).map { x =>
+    x + 1
+  }
+
+  // syntax sugar #2: Single abstract method
+  trait Action {
+    def act(x: Int): Int
+  }
+
+  val anInstance: Action = new Action {
+    override def act(x: Int): Int = x + 1
+  }
+
+  val aFunkyInstance: Action = (x: Int) => x + 1 // magic
+
+  // example: Runnables
+  val aThread = new Thread( new Runnable {
+    override def run(): Unit = println("hello, Scala!")
+  })
+
+  val aSweeterThread = new Thread(() => println("sweet, Scala!"))
+
+  abstract class AnAbstractType {
+    def implemented: Int = 23
+    def f(a: Int): Unit
+  }
+
+  val anAbstractInstance: AnAbstractType = (a: Int) => println("sweet")
+
+
+  // syntax sugar #3: the :: and #:: methods are special
+
+  val prependedList = 2 :: List(3, 4)
+  // 2.::(List(3, 4))
+  // List(3, 4).::(2)
+  // ?!
+
+  // scala spec: last char decides associativity of method
+  1 :: 2 :: 3 :: List(4, 5)
+  List(4, 5).::(3).::(2).::(1)
+
+  class MyStream[T] {
+    def -->:(value: T): MyStream[T] = this // actual implementation here
+  }
+
+  val myStream = 1 -->: 2 -->: 3 -->: new MyStream[Int]
+
+
+  // syntax sugar #4: multi-word method naming
+  class TeenGirl(name: String) {
+    def `and then said`(gossip: String) = println(s"$name said $gossip")
+  }
+
+  val lilly = new TeenGirl("lilly")
+  lilly `and then said` "Scala is so sweet!"
+
+
+  // syntax sugar #5: infix types
+  class Composite[A, B]
+  val composite: Composite[Int, String] = ???
+  val compos: Int Composite String = ???
+
+  class -->[A, B]
+  val towards: Int --> String = ???
+
+
+  // syntax sugar #6: update() is veri special, much like apply()
+  val anArray = Array(1, 2, 3)
+  anArray(2) = 7 // rewritten to anArray.update(2, 7)
+  // used in mutable collections
+  // remember apply() AND update()!
+
+
+  // syntax sugar #7: setters for mutable containers
+  class Mutable {
+    private var internalMember: Int = 0 // private for OD encapsulation
+    def member: Int = internalMember // "getter"
+    def member_=(value: Int): Unit =
+      internalMember = value // "setter"
+  }
+
+  val aMutableContainer = new Mutable
+  aMutableContainer.member = 42 // rewritten to aMutableContainer.member_=(42)
+
+
+}
